0a854d4ec3e8819a5bf689d88a2e092f254b26d8
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-05 11:49:22 +0500

    update bot schema

--- src/bot/model/bot.schema.ts
+++ src/bot/model/bot.schema.ts
@@ -5,11 +5,30 @@ export type BotDocument = HydratedDocument<Bot>;
 
 @Schema()
 export class Bot {
-    @Prop()
-    chatId: string;
+  @Prop()
+  chatId: string;
 
-    @Prop()
-    firstName: string;
+  @Prop()
+  firstName: string;
+
+  @Prop({ default: 0 })
+  score: number;
+
+  @Prop({ default: 0 })
+  testScore: number; 
+
+  @Prop({ default: 0 })
+  testStep: number; 
+
+  @Prop({ default: false })
+  isTesting: boolean; 
+  
+  @Prop({ default: null })
+  currentQuestion: string;
+
+  @Prop({ default: null })
+  currentAnswer: number;
 }
+
 
 export const BotSchema = SchemaFactory.createForClass(Bot);
